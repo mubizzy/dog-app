8cc2480be3e69845d5321784bfa1e6b9c58565e8
unknown <ajibolamubarak304@gmail.com>
2022-09-19 14:16:49 +0100

    app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SingleDog from "./pages/SingleDog";
 
 const App = () => {
   return (
