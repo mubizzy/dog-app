b961f5606f4358aa09548806c8abeca2334bc593
unknown <ajibolamubarak304@gmail.com>
2022-09-19 14:33:29 +0100

    Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,23 @@
 import React from "react";
 
 const Home = () => {
+  useEffect(() => {
+    const fetchDogData = async()=>{
+        try{
+        const res = await fetch("https://api.thedogapi.com/v1/breeds")
+        const data = await res.json();
+        console.log(data)
+        setDogs(data)
+        }catch(error){
+        console.error(error)
+        }
+        }
+
+    }
+  
+    
+  }, [third])
+     
   return <div>Home</div>;
 };
 
