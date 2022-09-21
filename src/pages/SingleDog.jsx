d296bd078dc0b542d3d192a3486e6df1514da3c5
unknown <ajibolamubarak304@gmail.com>
2022-09-21 12:57:18 +0100

    singledog

--- src/pages/SingleDog.jsx
+++ src/pages/SingleDog.jsx
@@ -1,6 +1,20 @@
 import React from "react";
 
 const SingleDog = () => {
+  useEffect(() => {
+    const fetchDogData = async () => {
+      try {
+        const res = await fetch("https://api.thedogapi.com/v1/breeds");
+        const data = await res.json();
+        console.log(data);
+        setDogs(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchDogData();
+  }, []);
   return <div>SingleDog</div>;
 };
 
