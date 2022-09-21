fd3af9e5598038d7f7d80081a3a11f8ec1d3501f
unknown <ajibolamubarak304@gmail.com>
2022-09-21 13:00:57 +0100

    singledog

--- src/pages/SingleDog.jsx
+++ src/pages/SingleDog.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const SingleDog = () => {
+  const [dog, setDog] = useState(null);
   useEffect(() => {
     const fetchDogData = async () => {
       try {
         const res = await fetch("https://api.thedogapi.com/v1/breeds");
         const data = await res.json();
         console.log(data);
-        setDogs(data);
+        setDog(data);
       } catch (error) {
         console.error(error);
       }
