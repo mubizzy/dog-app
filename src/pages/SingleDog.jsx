bb78c23a8c869a56880a443e3c2da3f5d32bb700
unknown <ajibolamubarak304@gmail.com>
2022-09-22 12:28:08 +0100

    single

--- src/pages/SingleDog.jsx
+++ src/pages/SingleDog.jsx
@@ -36,7 +36,7 @@ const SingleDog = () => {
             </article>
             <article>
               <h1 className="text-3xl font-bold text-white mb-8 lg:text-5xl">
-                {item.name}
+                {doggy.name}
               </h1>
             </article>
           </div>
